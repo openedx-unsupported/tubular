14867be57ce18056702ebd3e076b5be6e8825d1f
John Eskew <jeskew@edx.org>
2016-01-22 17:43:13 -0500

    Add GoCD-specific code

---
+++ tubular/gocd/trigger_pipeline.py
@@ -0,0 +1,38 @@
+"""
+Trigger a GoCD pipeline for a particular GitHub repo/PR.
+"""
+import os
+import sys
+import click
+from gocd_api_utils import GoCDApiUtils
+
+
+@click.command()
+@click.option('--pipeline_name',
+              help="Name of pipeline to trigger.",
+              required=True,
+              )
+@click.option('--repo',
+              default='edx/dummy-webapp',
+              help="GitHub repository - in the form 'org/repo'.",
+              required=True,
+              )
+@click.option('--pr_id',
+              default=None,
+              help="ID of GitHub pull request containing code to merge.",
+              type=int,
+              required=True,
+              )
+def cli(pipeline_name, repo, pr_id):
+    """
+    Trigger a GoCD pipeline using the specified GitHub repo/PR/branch.
+    """
+    success = GoCDApiUtils().trigger_pipeline(pipeline_name, repo, pr_id)
+    if success:
+        print "Schedule of pipeline {} for repo {} and PR {}: success.".format(pipeline_name, repo, pr_id)
+    # An exit code of 0 means success and non-zero means failure.
+    sys.exit(not success)
+
+
+if __name__ == '__main__':
+    cli()
