a68bb5de7456b1a55ab219efb2c35c6f6bbddbec
Feanil Patel <feanil@edx.org>
2016-01-25 15:25:37 -0500

    Add check for tests into pipeline.

--- jenkins_flow/Pipeline.groovy
+++ jenkins_flow/Pipeline.groovy
@@ -1,25 +1,43 @@
+import groovy.json.JsonSlurper
+
 node {
-    echo "Poll SCM"
+    env.REPO_ID = 49974806
+    echo "Check to see if the PR is against the correct branch."
     sh "virtualenv venv"
     git url: "https://github.com/edx/tubular.git", branch: "${TUBULAR_BRANCH}"
-    sh ". venv/bin/activate; pip install -r scripts/github/requirements.pip; python scripts/github/check_pr_against_branch.py -r 49974806 -p ${PR_NUMBER} -b ${LIVE_BRANCH}"
+    sh ". venv/bin/activate; pip install -r scripts/github/requirements.pip; python scripts/github/check_pr_against_branch.py -r ${env.REPO_ID} -p ${PR_NUMBER} -b ${LIVE_BRANCH}"
+
+    echo "PR is passing so grabbing the hash."
+    def api = new URL("https://api.github.com/repos/edx/dummy-webapp/pulls/${PR_NUMBER}")
+    def pr = new JsonSlurper().parseText(api.text)
+    env.PR_SHA = pr.head.sha
+
 }
 
-parallel([ "foo":
-{node {
-    echo "Node 1"
-    sleep 30
-}},
-"bar":
-{node {
-    echo "Node 2"
-    sleep 30
-}}])
+
+parallel([
+    "foo": {
+        node {
+            echo "Check to make sure commit has passed tests."
+            sh "virtualenv venv"
+            git url: "https://github.com/edx/tubular.git", branch: "${TUBULAR_BRANCH}"
+            sh ". venv/bin/activate; pip install -r scripts/github/requirements.pip; python scripts/github/check_pr_tests_status.py -c ${env.PR_SHA} -r ${env.REPO_ID}"
+        }
+    },
+    "bar": {
+        node {
+            echo "Node 2"
+            sleep 30
+        }
+    }
+])
 
 node {
     echo "Build step."
     echo "Collect some artifacts"
-    sleep 5
+    #    git url: "${DUMMY_WEBAPP_URL}", branch: env.PR_SHA
+    sh "git clone ${DUMMY_WEBAPP_URL} dummy_webapp; cd dummy_webapp; git checkout ${env.PR_SHA}"
+    sh "ls -al"
 }
 
 node {
