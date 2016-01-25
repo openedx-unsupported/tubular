1de3e2a68b7090f8421d25635662c405049aceb8
John Eskew <jeskew@edx.org>
2016-01-25 12:15:06 -0500

    Add versions to required modules. Refer to tubular itself from requirements.pip.

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup
 
 setup(
     name='tubular',
-    version='0.1',
+    version='0.0.1',
     description='Continuous Delivery scripts for pipeline evaluation',
     packages=['tubular', 'tubular.gocd', 'tubular.scripts', 'tubular.scripts.github', 'tubular.scripts.hipchat'],
     install_requires=[
-        'click',
-        'requests',
+        'click>=6.2',
+        'requests>=2.9',
     ],
 )
