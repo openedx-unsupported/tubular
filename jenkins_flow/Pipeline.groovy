251cecefe90aea0bf9bdc306df591e11513b3357
Feanil Patel <feanil@edx.org>
2016-01-27 17:48:32 -0500

    Update to checkout git repos in to seperate directories.

--- jenkins_flow/Pipeline.groovy
+++ jenkins_flow/Pipeline.groovy
@@ -13,6 +13,20 @@ def fail(msg, color = "red") {
     error(msg)
 }
 
+
+def _checkout( repo_url, branch, directory_name){
+    checkout([
+        $class: 'GitSCM',
+        branches: [[name: "${branch}"]],
+        doGenerateSubmoduleConfigurations: false,
+        extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: "${directory_name}"]],
+        submoduleCfg: [],
+        userRemoteConfigs: [[url: "${repo_url}"]]
+    ])
+}
+
+def TUBULAR_REPO_URL = "https://github.com/edx/tubular.git"
+
 node {
     notify("Starting dummy-deploy pipeline to deploy PR #${PR_NUMBER} to dummy-environment")
 }
@@ -21,10 +35,10 @@ node {
     env.REPO_ID = 49974806
     notify("Checking to see if the PR(#${PR_NUMBER}) is against the live branch(${LIVE_BRANCH}).")
     sh "virtualenv venv"
-    git url: "https://github.com/edx/tubular.git", branch: "${TUBULAR_BRANCH}"
+    _checkout("${TUBULAR_REPO_URL}", "${TUBULAR_BRANCH}", "tubular")
 
     try {
-        sh ". venv/bin/activate; pip install -r scripts/github/requirements.pip; python scripts/github/check_pr_against_branch.py -r ${env.REPO_ID} -p ${PR_NUMBER} -b ${LIVE_BRANCH}"
+        sh ". venv/bin/activate; pip install -r tubular/scripts/github/requirements.pip; python tubular/scripts/github/check_pr_against_branch.py -r ${env.REPO_ID} -p ${PR_NUMBER} -b ${LIVE_BRANCH}"
     } catch (e) {
         fail("PR is not against ${LIVE_BRANCH}")
     }
@@ -40,10 +54,10 @@ parallel([
         node {
             notify("Check to make sure commit has passed tests.")
             sh "virtualenv venv"
-            git url: "https://github.com/edx/tubular.git", branch: "${TUBULAR_BRANCH}"
-	    try {
-                sh ". venv/bin/activate; pip install -r scripts/github/requirements.pip; python scripts/github/check_pr_tests_status.py -c ${env.PR_SHA} -r ${env.REPO_ID}"
-	    } catch (e) {
+        _checkout("${TUBULAR_REPO_URL}", "${TUBULAR_BRANCH}", "tubular")
+        try {
+                sh ". venv/bin/activate; pip install -r tubular/scripts/github/requirements.pip; python tubular/scripts/github/check_pr_tests_status.py -c ${env.PR_SHA} -r ${env.REPO_ID}"
+        } catch (e) {
                 fail("PR failed some tests.  Please check https://github.com/${DUMMY_WEBAPP_ORG}/${DUMMY_WEBAPP_NAME}/pull/${PR_NUMBER}")
             }
             notify("Commit(${env.PR_SHA}) has passed all tests.")
@@ -54,19 +68,20 @@ parallel([
             echo "Node 2"
             sleep 30
         }
-    }
-])
+    },
+   "failFast": true])
 
 node {
     echo "Build step."
     notify("Building artifact. Eventually AMI, just an archive for now.")
-    sh "git clone https://github.com/${DUMMY_WEBAPP_ORG}/${DUMMY_WEBAPP_NAME}.git; cd ${DUMMY_WEBAPP_NAME}; git checkout ${env.PR_SHA}"
+    sh "ls -al"
+    _checkout("https://github.com/${DUMMY_WEBAPP_ORG}/${DUMMY_WEBAPP_NAME}.git", "${env.PR_SHA}", "${DUMMY_WEBAPP_NAME}")
     sh "ls -al"
 }
 
 node {
-    sleep 5
     notify("Waiting for user input before we deploy (#${PR_NUMBER}:${env.PR_SHA}) to dummy environment.\n${env.BUILD_URL}/console")
+    sleep 5
     input "Ready to deploy?"
 }
 
