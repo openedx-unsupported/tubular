1fe01d4564b5fb71927076a151199716c7db5092
Feanil Patel <feanil@edx.org>
2016-02-09 15:19:49 -0500

    Get all ASGs that match an EDC.

--- tubular/ec2.py
+++ tubular/ec2.py
@@ -42,3 +42,69 @@ def edc_for_ami(ami_id):
 
     LOG.debug("Got EDC for {}: {}".format(ami_id, edc))
     return edc
+
+def asgs_for_edc(edc):
+    """
+    All AutoScalingGroups that have the tags of this cluster.
+
+    A cluster is made up of many auto_scaling groups.
+
+    Input: An EDC Tuple.
+    Returns: An iterable of cluster names that match an EDC.
+    eg.
+
+     [
+         u'edxapp-v018',
+         u'sandbox-edx-hacking-ASG',
+         u'sandbox-edx-insights-ASG',
+         u'test-edx-ecomapp',
+         u'test-edx-edxapp-v007',
+         u'test2-edx-certificates',
+     ]
+
+    """
+    autoscale = boto.connect_autoscale()
+    all_groups = autoscale.get_all_groups()
+    LOG.debug("All groups: {}".format(all_groups))
+    for group in all_groups:
+        tags = dict_from_tag_list(group.tags)
+        LOG.debug("Tags for asg {}: {}".format(group.name, tags))
+        if all([ tag in tags for tag in ['environment', 'deployment', 'cluster']]):
+            group_env = tags['environment']
+            group_deployment = tags['deployment']
+            group_cluster = tags['cluster']
+
+            group_edc = EDC(group_env, group_deployment, group_cluster)
+
+            if group_edc == edc:
+                yield group.name
+
+def dict_from_tag_list(tag_list):
+    """
+    Take a list of tags and convert it to a dict mapping the
+    tag names to the tag values.
+
+    Input:
+    [
+        Tag(Name=ZZZZZTest-edx-ecommerce),
+        Tag(cluster=ecommerce),
+        Tag(deployment=edx),
+        Tag(environment=zzzzzzzzz),
+        Tag(services=ecommerce),
+    ]
+
+    Returns: A dictionary mapping tag names to tag values.
+    {
+        "Name": "ZZZZZTest-edx-ecommerce",
+        "cluster": "ecommerce",
+        "deployment": "edx",
+        "environment": "zzzzzzzzz",
+        "services": "ecommerce",
+    }
+    """
+    tag_dict = {}
+    for item in tag_list:
+        tag_dict[item.key] = item.value
+
+    return tag_dict
+
