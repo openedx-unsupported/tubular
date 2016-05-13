ed836debfdc97e77a86494f19e9238217c6f6d59
Brian Beggs <bbeggs@edx.org>
2016-05-13 17:03:44 -0400

    Script to delete an ASG

---
+++ scripts/delete-asg.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+import sys
+import logging
+import traceback
+import click
+from os import path
+
+# Add top-level module path to sys.path before importing tubular code.
+sys.path.append( path.dirname( path.dirname( path.abspath(__file__) ) ) )
+
+from tubular import asgard
+
+
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+
+@click.command()
+@click.option('--asg_name', envvar='ASG_NAME', help='the name of the Autoscale Group to delete', required=True)
+def delete_asg(asg_name):
+    try:
+        asgard.delete_asg(asg_name)
+    except Exception, e:
+        traceback.print_exc()
+        click.secho("Error Deploying AMI: {0}.\nMessage: {1}".format(asg_name, e.message), fg='red')
+        sys.exit(1)
+
+    sys.exit(0)
+
+if __name__ == "__main__":
+    delete_asg()
