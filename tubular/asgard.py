1883db58d286014755f36d890d58605025004f78
Feanil Patel <feanil@edx.org>
2016-05-16 15:29:37 -0400

    5 minutes is not enough time sometimes.
    See https://gocd.tools.edx.org/go/tab/build/detail/deploy-edx-ami/108/Deploy_AMI/1/Deploy_AMI

--- tubular/asgard.py
+++ tubular/asgard.py
@@ -14,7 +14,7 @@ from exception import BackendError, ASGDoesNotExistException
 
 ASGARD_API_ENDPOINT = os.environ.get("ASGARD_API_ENDPOINTS", "http://dummy.url:8091/us-east-1")
 ASGARD_API_TOKEN = "asgardApiToken={}".format(os.environ.get("ASGARD_API_TOKEN", "dummy-token"))
-ASGARD_WAIT_TIMEOUT = int(os.environ.get("ASGARD_WAIT_TIMEOUT", 300))
+ASGARD_WAIT_TIMEOUT = int(os.environ.get("ASGARD_WAIT_TIMEOUT", 600))
 REQUESTS_TIMEOUT = float(os.environ.get("REQUESTS_TIMEOUT", 10))
 
 
