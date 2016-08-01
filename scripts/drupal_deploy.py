0370608f5c4603b653faee2f0b456adb82bd9508
Dillon Dumesnil <dillon.dumesnil@gmail.com>
2016-08-01 10:17:01 -0400

    Added in path before importing tubular code and changed fetch_deployed_tag to write to the file.

--- scripts/drupal_deploy.py
+++ scripts/drupal_deploy.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python
+import sys
+from os import path
 import click
+
+# Add top-level module path to sys.path before importing tubular code.
+sys.path.append( path.dirname( path.dirname( path.abspath(__file__) ) ) )
+
 from tubular import drupal
 
 
 @click.command()
-@click.option("--env", help="The environment to clear varnish caches in.", type=str, required=True)
+@click.option("--env", help="The environment to deploy code in.", type=str, required=True)
 @click.option("--username", help="The Acquia username necessary to run the command.", type=str, required=True)
 @click.option("--password", help="The Acquia password necessary to run the command.", type=str, required=True)
 @click.option("--tag", help="The tag name to be deployed to the environment.", type=str, required=True)
--- scripts/drupal_fetch_deployed_tag.py
+++ scripts/drupal_fetch_deployed_tag.py
@@ -1,22 +1,30 @@
 #!/usr/bin/env python
+import sys
+from os import path
 import click
+
+# Add top-level module path to sys.path before importing tubular code.
+sys.path.append( path.dirname( path.dirname( path.abspath(__file__) ) ) )
+
 from tubular import drupal
 
 
 @click.command()
-@click.option("--env", help="The environment to clear varnish caches in.", type=str, required=True)
+@click.option("--env", help="The environment to fetch the current tag name in.", type=str, required=True)
 @click.option("--username", help="The Acquia username necessary to run the command.", type=str, required=True)
 @click.option("--password", help="The Acquia password necessary to run the command.", type=str, required=True)
-def fetch_deployed_tag(env, username, password):
+@click.option("--path_name", help="The path to write the tag name to", type=str, required=True)
+def fetch_deployed_tag(env, username, password, path_name):
     """
     Fetches the currently deployed tag in the given environment
 
     Args:
-        env (str): The environment to clear varnish caches in (e.g. test or prod)
+        env (str): The environment to fetch the current tag name in (e.g. test or prod)
         username (str): The Acquia username necessary to run the command.
         password (str): The Acquia password necessary to run the command.
+        path_name (str): The path to write the tag name to.
     """
-    drupal.fetch_deployed_tag(env, username, password)
+    drupal.fetch_deployed_tag(env, username, password, path_name)
 
 if __name__ == "__main__":
     fetch_deployed_tag()
