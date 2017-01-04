494cc0ff5af9101c34cb53c395fceb48cfdc616c
Jesse Zoldak <zoldak@edx.org>
2017-01-04 15:34:40 -0500

    Fix unicode strings to clean up output for the jenkins script

--- scripts/jenkins_trigger_build.py
+++ scripts/jenkins_trigger_build.py
@@ -1,8 +1,6 @@
 """
 Command-line script to trigger a jenkins job
 """
-from __future__ import unicode_literals
-
 import logging
 from os import path
 import sys
@@ -17,59 +15,57 @@ from tubular import jenkins  # pylint: disable=wrong-import-position
 
 @click.command()
 @click.option(
-    "--url",
-    help="The base jenkins URL. E.g. https://test-jenkins.testeng.edx.org",
+    u"--url",
+    help=u"The base jenkins URL. E.g. https://test-jenkins.testeng.edx.org",
     type=str,
     required=True,
-    default="https://test-jenkins.testeng.edx.org"
+    default=u"https://test-jenkins.testeng.edx.org"
 )
 @click.option(
-    "--user_name",
-    help="The Jenkins username for triggering the job.",
-    type=str,
-    required=True,
-)
-@click.option(
-    "--user_token",
-    help="API token for the user. Available at {url}/user/{user_name)/configure",
-    type=str,
-    required=True,
-    envvar='JENKINS_USER_TOKEN'
-)
-@click.option(
-    "--job",
-    help="The name of the jenkins job. E.g. test-project",
+    u"--user_name",
+    help=u"The Jenkins username for triggering the job.",
     type=str,
     required=True,
 )
 @click.option(
-    "--token",
-    help="The authorization token for the job. Must match that configured in the job definition.",
+    u"--user_token",
+    help=u"API token for the user. Available at {url}/user/{user_name)/configure",
     type=str,
     required=True,
-    envvar='JENKINS_JOB_TOKEN'
+    envvar=u'JENKINS_USER_TOKEN'
 )
 @click.option(
-    "--cause",
-    help="Text that will be included in the recorded build cause.",
+    u"--job",
+    help=u"The name of the jenkins job. E.g. test-project",
+    type=str,
+    required=True,
+)
+@click.option(
+    u"--token",
+    help=u"The authorization token for the job. Must match that configured in the job definition.",
+    type=str,
+    required=True,
+    envvar=u'JENKINS_JOB_TOKEN'
+)
+@click.option(
+    u"--cause",
+    help=u"Text that will be included in the recorded build cause.",
     type=str,
     required=False,
 )
 @click.option(
-    '--param',
-    help=u'Key/value pairs to pass to the job as parameters. E.g. --param FOO bar --param BAZ biz',
+    u"--param",
+    help=u"Key/value pairs to pass to the job as parameters. E.g. --param FOO bar --param BAZ biz",
     multiple=True,
     required=False,
     type=(str, str)
 )
 def trigger(url, user_name, user_token, job, token, cause, param):
-    """
-    Trigger a jenkins job.
-    """
+    u"""Trigger a jenkins job. """
     jenkins.trigger_build(url, user_name, user_token, job, token, cause, param)
 
-if __name__ == "__main__":
-    # Configure logging so that the tubular module methods will
-    # output to stdout of the console that called this script.
+if __name__ == u"__main__":
+    # Configure logging for the tubular module methods to
+    # print to stdout of the console that called this script.
     logging.basicConfig(stream=sys.stdout, level=logging.INFO)
     trigger()  # pylint: disable=no-value-for-parameter
