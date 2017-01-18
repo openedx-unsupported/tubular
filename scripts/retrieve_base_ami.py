c28dc4f29fd73730da029b838a3e1ac5ae3ce6da
Calen Pennington <cale@edx.org>
2017-01-18 11:52:22 -0500

    Capture tags for the base AMIs

--- scripts/retrieve_base_ami.py
+++ scripts/retrieve_base_ami.py
@@ -23,30 +23,46 @@ logging.basicConfig(level=logging.INFO)
 @click.option(
     '--environment', '-e',
     help='Environment for AMI, e.g. prod, stage',
-    required=True
 )
 @click.option(
     '--deployment', '-d',
     help='Deployment for AMI e.g. edx, edge',
-    required=True
 )
 @click.option(
     '--play', '-p',
     help='Play for AMI, e.g. edxapp, insights, discovery',
-    required=True
+)
+@click.option(
+    '--override',
+    help='Override AMI id to use',
 )
 @click.option(
     '--out_file',
     help='Output file for the AMI information yaml.',
     default=None
 )
-def retrieve_base_ami(environment, deployment, play, out_file):
+def retrieve_base_ami(environment, deployment, play, override, out_file):
     """
     Method used to retrieve the last base AMI ID used for an environment/deployment/play.
     """
+
+    has_edp = environment is not None or deployment is not None or play is not None
+    if has_edp and override is not None:
+        logging.error("--environment, --deployment and --play are mutually exclusive with --override.")
+        sys.exit(1)
+
+    if not has_edp and override is None:
+        logging.error("Either --environment, --deployment and --play or --override are required.")
+        sys.exit(1)
+
     try:
-        ami_id = ec2.active_ami_for_edp(environment, deployment, play)
+        if override:
+            ami_id = override
+        else:
+            ami_id = ec2.active_ami_for_edp(environment, deployment, play)
+
         ami_info = {'base_ami_id': ami_id}
+        ami_info.update(ec2.tags_for_ami(ami_id))
         logging.info("Found active AMI ID for {env}-{dep}-{play}: {ami_id}".format(
             env=environment, dep=deployment, play=play, ami_id=ami_id
         ))
