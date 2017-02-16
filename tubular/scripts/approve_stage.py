34673596f1e803a3fd176288bc83ea6db0653d50
Feanil Patel <feanil@edx.org>
2017-02-16 15:45:40 -0500

    Update based on review.

--- tubular/scripts/approve_stage.py
+++ tubular/scripts/approve_stage.py
@@ -9,44 +9,39 @@ from __future__ import absolute_import
 from __future__ import unicode_literals
 from __future__ import print_function
 
-import os
-import sys
 import click
 from yagocd import Yagocd
-
-# Add top-level module path to sys.path before importing tubular code.
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
 @click.command()
 @click.option(
     '--go-server-url',
-    envvar='GOCD_SERVER_URL',
+    required=True,
     help='The URL for the GoCD server to interact with.',
 )
 @click.option(
     '--username',
-    envvar='GOCD_AUTOMATION_USER_NAME',
+    required=True,
     help='The username of the user to use when hitting the GoCD API.',
 )
 @click.option(
     '--secret',
-    envvar='GOCD_AUTOMATION_USER_SECRET',
+    required=True,
     help='The secret to use for auth with the GoCD API.',
 )
 @click.option(
     '--pipeline-name',
-    envvar='GOCD_PIPELINE_NAME',
+    required=True,
     help='The name of the pipeline to affect.',
 )
 @click.option(
     '--pipeline-counter',
-    envvar='GOCD_PIPELINE_COUNTER',
+    required=True,
     help='The number associated with this particular run of the pipeline.',
 )
 @click.option(
     '--stage-name',
-    envvar='GOCD_STAGE_NAME',
+    required=True,
     help='The stage that requires "manual" approval.',
 )
 def approve_stage(go_server_url, username, secret, pipeline_name, pipeline_counter, stage_name):
