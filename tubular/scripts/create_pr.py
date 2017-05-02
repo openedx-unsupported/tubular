2e352b4ce0c73d2c7d5c6f6c470bfa6f81fcfedd
John Eskew <jeskew@edx.org>
2017-05-02 14:19:14 -0400

    Remove PyGithub leakage - add custom exception when PR create fails.

--- tubular/scripts/create_pr.py
+++ tubular/scripts/create_pr.py
@@ -18,8 +18,7 @@ import yaml
 # Add top-level module path to sys.path before importing tubular code.
 sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
 
-from tubular.github_api import GitHubAPI  # pylint: disable=wrong-import-position
-from github.GithubException import GithubException  # pylint: disable=wrong-import-position
+from tubular.github_api import GitHubAPI, PullRequestCreationError  # pylint: disable=wrong-import-position
 
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 LOG = logging.getLogger(__name__)
@@ -75,37 +74,8 @@ def create_pull_request(org,
                         output_file):
     """
     Creates a pull request to merge a source branch into a target branch, if needed.
-    Both the source and target branches are assumed to already exist.
-
-    Args:
-        org (str):
-        repo (str):
-        token (str):
-        source_branch (str):
-        target_branch (str):
-        title (str):
-        body (str):
-        output_file (str):
-
-    Outputs a yaml file with information about the newly created PR:
-     ---
-    pr_created: true
-    pr_id: 96786312
-    pr_number: 3
-    pr_url: https://api.github.com/repos/macdiesel412/Rainer/pulls/3
-    pr_repo_url: /repos/macdiesel412/Rainer
-    pr_head: af538da6b229cf1dfa33d0171e75fbff6de4c283
-    pr_base: 2a800083658e2f5d11d1d40118024f77c59d1b9a
-    pr_diff_url: https://github.com/macdiesel412/Rainer/pull/3.diff
-    pr_mergable: null
-    pr_state: open
-    pr_mergable_state: unknown
-
-    -or-
-     ---
-    pr_created: false
-
-    if no PR is required.
+    Both the source and target branches are assumed to already exist. Outputs a YAML
+    file with information about the PR creation.
     """
     github_api = GitHubAPI(org, repo, token)
 
@@ -143,7 +113,7 @@ def create_pull_request(org,
                 title=title,
                 body=body
             )
-        except GithubException:
+        except PullRequestCreationError:
             LOG.error("Unable to create pull request. Aborting")
             raise
 
