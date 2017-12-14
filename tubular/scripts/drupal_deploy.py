aca16f139c66775d042fd63bba886e9216bc2912
Dillon-Dumesnil <dillon.dumesnil@gmail.com>
2017-12-14 16:39:25 -0500

    Revert "Adding in all other acquia environments. Changing language to make it…"

--- tubular/scripts/drupal_deploy.py
+++ tubular/scripts/drupal_deploy.py
@@ -20,9 +20,8 @@ from tubular import drupal  # pylint: disable=wrong-import-position
 @click.option("--env", help="The environment to deploy code in.", type=str, required=True)
 @click.option("--username", help="The Acquia username necessary to run the command.", type=str, required=True)
 @click.option("--password", help="The Acquia password necessary to run the command.", type=str, required=True)
-@click.option("--branch_or_tag", help="The branch or tag name to be deployed to the environment.",
-              type=str, required=True)
-def deploy(env, username, password, branch_or_tag):
+@click.option("--tag", help="The tag name to be deployed to the environment.", type=str, required=True)
+def deploy(env, username, password, tag):
     """
     Deploys a given tag to the specified environment.
 
@@ -30,9 +29,9 @@ def deploy(env, username, password, branch_or_tag):
         env (str): The environment to deploy code in (e.g. test or prod)
         username (str): The Acquia username necessary to run the command.
         password (str): The Acquia password necessary to run the command.
-        branch_or_tag (str): The branch or Ctag to deploy to the specified environment.
+        tag (str): The tag to deploy to the specified environment.
     """
-    drupal.deploy(env, username, password, branch_or_tag)
+    drupal.deploy(env, username, password, tag)
 
 if __name__ == "__main__":
     deploy()  # pylint: disable=no-value-for-parameter
