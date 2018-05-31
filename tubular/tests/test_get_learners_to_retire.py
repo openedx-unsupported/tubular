69639899b022e7bfaa732a185a4d251f911e2fd4
Brian Mesick <bmesick@edx.org>
2018-05-31 15:04:20 -0400

    Minimal tests to smoke out simple errors in get_learners_to_retire.py (#234)

---
+++ tubular/tests/test_get_learners_to_retire.py
@@ -0,0 +1,104 @@
+"""
+Test the get_learners_to_retire.py script
+"""
+
+import os
+from mock import patch, DEFAULT
+
+from click.testing import CliRunner
+from slumber.exceptions import HttpClientError, HttpServerError
+
+from tubular.scripts.get_learners_to_retire import (
+    get_learners_to_retire
+)
+from tubular.tests.retirement_helpers import fake_config_file
+
+
+def _call_script(expected_user_files, cool_off_days=1, output_dir='test'):
+    """
+    Call the retired learner script with the given username and a generic, temporary config file.
+    Returns the CliRunner.invoke results
+    """
+    runner = CliRunner()
+    with runner.isolated_filesystem():
+        with open('test_config.yml', 'w') as f:
+            fake_config_file(f)
+        result = runner.invoke(
+            get_learners_to_retire,
+            args=['--config_file', 'test_config.yml', '--cool_off_days', cool_off_days, '--output_dir', output_dir])
+        print(result)
+        print(result.output)
+
+        # This is the number of users in the mocked call, each should have a file if the number is
+        # greater than 0, otherwise a failure is expected and the output dir should not exist
+        if expected_user_files:
+            assert len(os.listdir(output_dir)) == expected_user_files
+        else:
+            assert not os.path.exists(output_dir)
+    return result
+
+
+@patch('tubular.edx_api.BaseApiClient.get_access_token')
+@patch.multiple(
+    'tubular.edx_api.LmsApi',
+    learners_to_retire=DEFAULT
+)
+def test_success(*args, **kwargs):
+    mock_get_access_token = args[0]
+    mock_get_learners_to_retire = kwargs['learners_to_retire']
+
+    mock_get_access_token.return_value = ('THIS_IS_A_JWT', None)
+    mock_get_learners_to_retire.return_value = [
+        {'original_username': 'test_user1'},
+        {'original_username': 'test_user2'},
+    ]
+
+    result = _call_script(2)
+
+    # Called once per API we instantiate (LMS, ECommerce, Credentials)
+    assert mock_get_access_token.call_count == 1
+    mock_get_learners_to_retire.assert_called_once()
+
+    assert result.exit_code == 0
+
+
+@patch('tubular.edx_api.BaseApiClient.get_access_token')
+@patch.multiple(
+    'tubular.edx_api.LmsApi',
+    learners_to_retire=DEFAULT
+)
+def test_lms_down(*args, **kwargs):
+    mock_get_access_token = args[0]
+    mock_get_learners_to_retire = kwargs['learners_to_retire']
+
+    mock_get_access_token.return_value = ('THIS_IS_A_JWT', None)
+    mock_get_learners_to_retire.side_effect = HttpServerError
+
+    result = _call_script(0)
+
+    # Called once per API we instantiate (LMS, ECommerce, Credentials)
+    assert mock_get_access_token.call_count == 1
+    mock_get_learners_to_retire.assert_called_once()
+
+    assert result.exit_code == -1
+
+
+@patch('tubular.edx_api.BaseApiClient.get_access_token')
+@patch.multiple(
+    'tubular.edx_api.LmsApi',
+    learners_to_retire=DEFAULT
+)
+def test_misconfigured(*args, **kwargs):
+    mock_get_access_token = args[0]
+    mock_get_learners_to_retire = kwargs['learners_to_retire']
+
+    mock_get_access_token.return_value = ('THIS_IS_A_JWT', None)
+    mock_get_learners_to_retire.side_effect = HttpClientError
+
+    result = _call_script(0)
+
+    # Called once per API we instantiate (LMS, ECommerce, Credentials)
+    assert mock_get_access_token.call_count == 1
+    mock_get_learners_to_retire.assert_called_once()
+
+    assert result.exit_code == -1
