e357169f53d764ae52deb2058e93833d6997c3fd
Joseph Mulloy <jmulloy@edx.org>
2020-02-24 17:06:16 -0500

    Add no-op mode for testing

--- tubular/scripts/message_prs_in_range.py
+++ tubular/scripts/message_prs_in_range.py
@@ -72,6 +72,11 @@ LOG = logging.getLogger(__name__)
 @click.option(
     u'--extra_text', u'extra_text', default=''
 )
+@click.option(
+    u'--no-op',
+    help=u'Disable posting messages for testing',
+    is_flag=True
+)
 def message_pull_requests(org,
                           repo,
                           token,
@@ -82,7 +87,8 @@ def message_pull_requests(org,
                           head_ami_tags,
                           head_ami_tag_app,
                           message_type,
-                          extra_text):
+                          extra_text,
+                          no_op):
     u"""
     Message a range of Pull requests between the BASE and HEAD SHA specified.
 
@@ -100,6 +106,7 @@ def message_pull_requests(org,
         head_ami_tag_app (str): the app name to read the head_ami_tags
         message_type (str): type of message to send
         extra_text (str): Extra text to be inserted in the PR message
+        no_op (bool): Disable posting comments for testing
 
     Returns:
         None
@@ -121,7 +128,7 @@ def message_pull_requests(org,
     LOG.info("Github API Rate Limit: {}".format(api.get_rate_limit()))
     pull_requests = retrieve_pull_requests(api, base_sha, head_sha)
     for pull_request in pull_requests:
-        message_pr(api, MessageType[message_type], pull_request, extra_text)
+        message_pr(api, MessageType[message_type], pull_request, extra_text, no_op)
 
 
 def get_client(org, repo, token):
@@ -161,7 +168,7 @@ def retrieve_pull_requests(api, base_sha, head_sha):
     return pull_requests
 
 
-def message_pr(api, message_type, pull_request, extra_text):
+def message_pr(api, message_type, pull_request, extra_text, no_op):
     u"""
     Send a Message for a Pull request.
 
@@ -177,13 +184,16 @@ def message_pr(api, message_type, pull_request, extra_text):
         None
     """
     LOG.info("Github API Rate Limit: {}".format(api.get_rate_limit()))
-    LOG.info(u"Posting message type %r to %d.", message_type.name, pull_request.number)
+    if no_op:
+        LOG.info(u"No-op mode: Whould have posted message type %r to %d.", message_type.name, pull_request.number)
+    else:
+        LOG.info(u"Posting message type %r to %d.", message_type.name, pull_request.number)
 
-    try:
-        api.message_pr_with_type(pr_number=pull_request, message_type=message_type, extra_text=extra_text)
-    except UnknownObjectException as exc:
-        LOG.error(u"message_pr_with_type args were: pr_number={0} message_type={1} extra_text={2}".format(pull_request, message_type, extra_text))
-        raise exc
+        try:
+            api.message_pr_with_type(pr_number=pull_request, message_type=message_type, extra_text=extra_text)
+        except UnknownObjectException as exc:
+            LOG.error(u"message_pr_with_type args were: pr_number={0} message_type={1} extra_text={2}".format(pull_request, message_type, extra_text))
+            raise exc
 
 
 if __name__ == u"__main__":
