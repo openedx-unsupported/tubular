5552662dc88884b53c4a466ef800a6b3403e548f
Sharon Wang <sharon.ran.wang@gmail.com>
2020-06-10 16:18:12 -0400

    lint and errors

--- tubular/opsgenie_api.py
+++ tubular/opsgenie_api.py
@@ -42,6 +42,13 @@ class OpsGenieAPI:
         return result
 
     def alert_opsgenie(self, message, description):
+        """
+        Alert team of an issue - such as GoCD pipeline failure
+
+        Arguments:
+            message: a summary of the issue
+            description: a more detailed description
+        """
         post_url = 'https://api.opsgenie.com/v2/alerts'
 
         alert_data = {
@@ -49,13 +56,12 @@ class OpsGenieAPI:
             'description': description,
         }
 
-        response = requests.post(post_url,
-                                 headers=headers,
-                                 data=json.dumps(alert_data)
-                                )
+        response = self.session.post(
+            url=post_url,
+            data=json.dumps(alert_data)
+        )
 
         if response.status_code not in (200, 201, 204):
             raise OpsgenieMessageSendFailure(
                 "Message {} failed: {}".format(message, response.text)
             )
-
