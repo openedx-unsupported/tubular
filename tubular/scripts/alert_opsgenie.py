5e3ad9a6ace575b3b1590a6198635891b339d6bb
Sharon Wang <sharon.ran.wang@gmail.com>
2020-06-10 17:03:08 -0400

    lint

--- tubular/scripts/alert_opsgenie.py
+++ tubular/scripts/alert_opsgenie.py
@@ -2,9 +2,9 @@ from tubular.opsgenie_api import OpsGenieAPI
 
 
 def alert_opsgenie(auth_token, message, description):
-	"""
-	Sends an alert to an opsgenie team
-	"""
+    """
+    Sends an alert to an opsgenie team
+    """
     opsgenie = OpsGenieAPI(auth_token)
 
     opsgenie.alert_opsgenie(message, description)
