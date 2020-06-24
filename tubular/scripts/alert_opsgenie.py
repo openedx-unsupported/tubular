4945ea8ce81236d0a7368da8e8e8c097c3206a54
Sharon Wang <sharon.ran.wang@gmail.com>
2020-06-24 14:26:32 -0400

    making args work?

--- tubular/scripts/alert_opsgenie.py
+++ tubular/scripts/alert_opsgenie.py
@@ -1,8 +1,27 @@
 """
 Command-line script to submit an alert to Opsgenie Api.
 """
+import click
+
 from tubular.opsgenie_api import OpsGenieAPI
 
+
+@click.command()
+@click.option(
+    '--auth_token',
+    required=True,
+    help="Authentication token to use for Opsgenie Alerts API.",
+)
+@click.option(
+    '--message',
+    required=True,
+    help="Message in the subject of Opsgenie Alert",
+)
+@click.option(
+    '--description',
+    required=True,
+    help="Message in the body of Opsgenie Alert",
+)
 
 def alert_opsgenie(auth_token, message, description):
     """
