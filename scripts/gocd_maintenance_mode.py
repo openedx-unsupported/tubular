7e72a120309a87e6f9feaac0d7dcb8df9a028090
syedimranhassan <imran.hassan@arbisoft.com>
2020-12-28 18:36:57 +0500

    Added script to change maintenance mode GoCD server

--- scripts/gocd_maintenance_mode.py
+++ scripts/gocd_maintenance_mode.py
@@ -16,40 +16,26 @@ logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 LOG = logging.getLogger(__name__)
 
 
-def enable_maintenance_mode(host, token):
+def switch_maintenance_mode(host, token, mode):
     """
     GoCD enable maintenance mode
     https://api.gocd.org/current/#enable-maintenance-mode
-    """
-    url = "https://{host}/go/api/admin/maintenance_mode/enable".format(
-        host=host)
-
-    headers = {
-        'Accept': 'application/vnd.go.cd.v2+json',
-        'Authorization': "bearer {token}".format(token=token),
-        'X-GoCD-Confirm': "true",
-    }
-    r = requests.get(url, headers=headers)
-    r.raise_for_status()
-    return r
-
-
-def disable_maintenance_mode(host, token):
-    """
     GoCD disable maintenance mode
     https://api.gocd.org/current/#disable-maintenance-mode
     """
-    url = "https://{host}/go/api/admin/maintenance_mode/disable".format(
-        host=host)
+    url = "https://{host}/go/api/admin/maintenance_mode/{mode}".format(
+        host=host, mode=mode)
 
     headers = {
-        'Accept': 'application/vnd.go.cd.v2+json',
+        'Accept': 'application/vnd.go.cd.v1+json',
         'Authorization': "bearer {token}".format(token=token),
-        'X-GoCD-Confirm': "true",
+        'X-GoCD-Confirm':"true",
+        'User-Agent': 'python-requests/2.25.1',
     }
-    r = requests.get(url, headers=headers)
+    r = requests.post(url, headers=headers)
     r.raise_for_status()
     return r
+
 
 def is_server_in_maintenance_mode(host, token):
     """
@@ -59,12 +45,12 @@ def is_server_in_maintenance_mode(host, token):
         host=host)
 
     headers = {
-        'Accept': 'application/vnd.go.cd.v2+json',
+        'Accept': 'application/vnd.go.cd.v1+json',
         'Authorization': "bearer {token}".format(token=token),
     }
     r = requests.get(url, headers=headers)
     r.raise_for_status()
-    return r
+    return r.json()
 
 
 @click.command()
@@ -76,23 +62,29 @@ def set_maintenance_mode_state(token, host, mode):
     Enable or Disbale maintenance mode on GoCD server
     """
     try:
-        logging.info(mode+"ing GoCD server maintenance mode ")
         if (mode == "enable"):
-            enable_maintenance_mode(host, token)
+            logging.info(mode[:-1]+"ing GoCD server maintenance mode ")
+            switch_maintenance_mode(host, token, mode)
+            logging.info("Sleeping for 10 sec")
+            time.sleep( 10 )
             flag = True
             while flag == True:
                 current_state = is_server_in_maintenance_mode(host, token)
-                if is_maintenance_mode == True:
-                    jobs_states = attributes["running_systems"]
-                    if not jobs_states["material_update_in_progress"] or not jobs_states["building_jobs"] or not jobs_states["scheduled_jobs"]:
+                if current_state["is_maintenance_mode"] == True:
+                    jobs_states = current_state["attributes"]["running_systems"]
+                    if not jobs_states["material_update_in_progress"] and not jobs_states["building_jobs"] and not jobs_states["scheduled_jobs"]:
+                        logging.info("All jobs are stopped. Server is now in maintenance mode")
                         break
                     else:
+                        logging.info("Some jobs are in progress. Sleeping for 60 sec")
                         time.sleep( 60 )
                         continue
-        elif (mode == "disbale"):
-            disable_maintenance_mode(host, token)
+        elif (mode == "disable"):
+            logging.info(mode[:-1]+"ing GoCD server maintenance mode ")
+            switch_maintenance_mode(host, token, mode)
         else:
-            print("Invalid mode")
+            logging.info("Invalid mode")
+            sys.exit(1)
     except Exception as err:  # pylint: disable=broad-except
         traceback.print_exc()
         click.secho('{}'.format(err), fg='red')
