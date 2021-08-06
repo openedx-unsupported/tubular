ceb9472bc71c005dd950b1bfe49f19cd9d851224
jinder1s <msingh@edx.org>
2021-08-06 10:08:42 -0400

    squash: quality

--- tubular/git_repo.py
+++ tubular/git_repo.py
@@ -87,7 +87,7 @@ class LocalGitAPI:
             push_ref = 'refs/heads/{}'.format(branch)
         push_info = self.repo.remotes[remote].push(push_ref, force=force)[0]
         if log_info:
-            LOG.info("push_info: flags: %d, %s", push_info.flag, push_info.summary)
+            LOG.info("push_info: flags: %d, %s", push_info.flags, push_info.summary)
             LOG.info("push_info: local_ref: %s, remote_ref %s", push_info.local_ref, push_info.remote_ref)
             LOG.info("push_info: %s", self.repo.remotes[remote].refs)
         return self._is_pushed(push_info)
