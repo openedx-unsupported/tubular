102fe852a9c06675bda1ad1bd609dd632029d972
ruzniaiedm <ruzniaievdm@gmail.com>
2022-04-13 14:54:07 +0300

    refactor: Remove EdxRestApiClient usage in tubular

--- tubular/tests/test_retire_one_learner.py
+++ tubular/tests/test_retire_one_learner.py
@@ -2,22 +2,19 @@
 Test the retire_one_learner.py script
 """
 
-from mock import patch, DEFAULT
-
 from click.testing import CliRunner
-from slumber.exceptions import HttpNotFoundError
+from mock import DEFAULT, patch
 
-from tubular.scripts.retire_one_learner import (
-    END_STATES,
-    ERR_BAD_CONFIG,
-    ERR_BAD_LEARNER,
-    ERR_SETUP_FAILED,
-    ERR_UNKNOWN_STATE,
-    ERR_USER_AT_END_STATE,
-    ERR_USER_IN_WORKING_STATE,
-    retire_learner
-)
-from tubular.tests.retirement_helpers import fake_config_file, get_fake_user_retirement
+from tubular.exception import HttpDoesNotExistException
+from tubular.scripts.retire_one_learner import (END_STATES, ERR_BAD_CONFIG,
+                                                ERR_BAD_LEARNER,
+                                                ERR_SETUP_FAILED,
+                                                ERR_UNKNOWN_STATE,
+                                                ERR_USER_AT_END_STATE,
+                                                ERR_USER_IN_WORKING_STATE,
+                                                retire_learner)
+from tubular.tests.retirement_helpers import (fake_config_file,
+                                              get_fake_user_retirement)
 
 
 def _call_script(username, fetch_ecom_segment_id=False):
@@ -130,7 +127,7 @@ def test_bad_learner(*args, **kwargs):
     mock_get_access_token.return_value = ('THIS_IS_A_JWT', None)
 
     # Broken API call, no state returned
-    mock_get_retirement_state.side_effect = HttpNotFoundError
+    mock_get_retirement_state.side_effect = HttpDoesNotExistException
     result = _call_script(username)
 
     assert mock_get_access_token.call_count == 3
@@ -354,7 +351,7 @@ def test_get_segment_id_not_found(*args, **kwargs):
     mock_get_retirement_state = kwargs['get_learner_retirement_state']
 
     mock_get_access_token.return_value = ('THIS_IS_A_JWT', None)
-    mock_get_tracking_key.side_effect = HttpNotFoundError('{} not found'.format(username))
+    mock_get_tracking_key.side_effect = HttpDoesNotExistException('{} not found'.format(username))
 
     mock_get_retirement_state.return_value = get_fake_user_retirement(
         original_username=username,
