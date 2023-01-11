03ec9ac045f66f30c35260757fedbf8c45c25e7c
Awais Qureshi <awais.qureshi@arbisoft.com>
2023-01-11 15:43:26 +0500

    feat: Adding new script to get all open prs with ready to merge label.

--- tubular/scripts/get_ready_to_merge_prs.py
+++ tubular/scripts/get_ready_to_merge_prs.py
@@ -1,24 +1,22 @@
 #! /usr/bin/env python3
 
 """
-Command-line script to create a release candidate for an application
+Command-line script to get open prs with label 'Ready to merge'
 """
-import io
-from os import path
-import sys
 import logging
+import sys
+from os import path
+
 import click
-import yaml
 import requests
-import json
 
 # Add top-level module path to sys.path before importing tubular code.
 sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
 
-from tubular.github_api import GitHubAPI
-
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 LOG = logging.getLogger(__name__)
+
+GIT_API_URL = 'https://api.github.com/search/issues?'
 
 
 @click.command("get_ready_to_merge_prs")
@@ -45,11 +43,9 @@ def get_ready_to_merge_prs(org, token):
     """
     LOG.info("Getting GitHub token...")
 
-    content = get_github_api_response(org, token)
-    if content:
-        data = json.loads(content)
-        for item in data['items']:
-            print(item['html_url'])
+    data = get_github_api_response(org, token)
+    if data is not None:
+        return [item['html_url'] for item in data['items']]
 
 
 def get_github_api_response(org, token):
@@ -57,19 +53,28 @@ def get_github_api_response(org, token):
     get github pull requests
     https://docs.github.com/en/search-github/searching-on-github/searching-issues-and-pull-requests
     """
-    url = 'https://api.github.com/search/issues?'
-
     params = 'q=is:pr is:open label:"Ready to merge" org:{org}'.format(org=org)
     headers = {
         'Accept': "application/vnd.github.antiope-preview+json",
         'Authorization': "bearer {token}".format(token=token),
     }
-    resp = requests.get(url, params=params, headers=headers)
-    content = None
-    if resp.status_code == 200:
-        content = resp.content.decode('utf-8')
+    data = []
 
-    return content
+    try:
+        resp = requests.get(GIT_API_URL, params=params, headers=headers)
+        if resp.status_code == 200:
+            data = resp.json()
+            return [item['html_url'] for item in data['items']]
+        else:
+            LOG.error(
+                'api return status code {code} and error {con}'.format(code=resp.status_code, con=resp.content)
+            )
+
+    except Exception as err:  # pylint: disable=broad-except
+        LOG.error('Github api throws error: {con}'.format(con=str(err)))
+
+    return data
+
 
 if __name__ == "__main__":
     get_ready_to_merge_prs()  # pylint: disable=no-value-for-parameter
---
+++ tubular/tests/test_ready_to_merge_prs.py
@@ -0,0 +1,75 @@
+"""
+Tests of the code to get open prs with label 'Ready to merge'
+"""
+import unittest
+from unittest import mock
+
+from tubular.scripts.get_ready_to_merge_prs import get_github_api_response
+
+
+class TestReadyToMergePRS(unittest.TestCase):
+    """
+    Tests cases for open prs with label 'Ready to merge'
+    """
+    def setUp(self):
+        super().setUp()
+        self.content = {
+            "total_count": 1,
+            "incomplete_results": "false",
+            "items": [
+                {
+                    "url": "https://api.github.com/repos/openedx/edx-toggles/issues/246",
+                    "repository_url": "https://api.github.com/repos/openedx/edx-toggles",
+                    "labels_url": "https://api.github.com/repos/openedx/edx-toggles/issues/246/labels{/name}",
+                    "comments_url": "https://api.github.com/repos/openedx/edx-toggles/issues/246/comments",
+                    "html_url": "https://github.com/openedx/edx-platform/pull/300001",
+                },
+                {
+                    "url": "https://api.github.com/repos/openedx/edx-toggles/issues/246",
+                    "repository_url": "https://api.github.com/repos/openedx/edx-toggles",
+                    "labels_url": "https://api.github.com/repos/openedx/edx-toggles/issues/246/labels{/name}",
+                    "comments_url": "https://api.github.com/repos/openedx/edx-toggles/issues/246/comments",
+                    "html_url": "https://github.com/openedx/edx-toggles/pull/2001",
+                }]
+        }
+
+    def _mock_response(self, status=200, json_data=None, raise_for_status=None):
+        """
+        mock the response.
+        """
+        mock_resp = mock.Mock()
+        mock_resp.raise_for_status = mock.Mock()
+        if raise_for_status:
+            mock_resp.raise_for_status.side_effect = raise_for_status
+
+        mock_resp.status_code = status
+
+        mock_resp.json = mock.Mock(return_value=json_data)
+        return mock_resp
+
+    @mock.patch('requests.get')
+    def test_apis(self, mock_get):
+        """ verify method returns the valid pr links """
+        mock_resp = self._mock_response(json_data=self.content)
+        mock_get.return_value = mock_resp
+        content = get_github_api_response('openedx', '12345')
+
+        self.assertEqual(
+            sorted(
+                ['https://github.com/openedx/edx-platform/pull/300001',
+                 'https://github.com/openedx/edx-toggles/pull/2001']
+            ), sorted(content)
+        )
+
+    @mock.patch('requests.get')
+    def test_apis_without_records(self, mock_get):
+        """ verify code works in case of no results """
+        mock_resp = self._mock_response(json_data={'total_count': 0, 'incomplete_results': False, 'items': []})
+        mock_get.return_value = mock_resp
+        content = get_github_api_response('openedx', '12345')
+        self.assertEqual([], content)
+
+    def test_apis_with_errors(self):
+        """ test in case of exception"""
+        with mock.patch('tubular.scripts.get_ready_to_merge_prs.requests.get', side_effect=Exception("error")):
+            get_github_api_response('openedx', '12345')
