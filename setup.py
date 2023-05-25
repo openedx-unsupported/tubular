5bd0b47e654171e195304d8e6157528f6cb14426
UsamaSadiq <usama7274@gmail.com>
2023-05-25 19:49:42 +0500

    build: update to python 3.10

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup
 
 setup(
     setup_requires=[u'pbr>=1.9', u'setuptools>=17.1'],
-    python_requires=">=3.6, <3.9",
+    python_requires=">=3.8, <3.10",
     pbr=True,
 )
