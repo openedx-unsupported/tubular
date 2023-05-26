cee63c83df255e13ca0de646c0398af8a8b15d40
UsamaSadiq <usama7274@gmail.com>
2023-05-26 11:13:01 +0500

    fix: update python constraint

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup
 
 setup(
     setup_requires=[u'pbr>=1.9', u'setuptools>=17.1'],
-    python_requires=">=3.8, <3.10",
+    python_requires=">=3.8, <3.11",
     pbr=True,
 )
